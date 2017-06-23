67ab5aa82c6e69dc750844a862799911fb0534e0
Skillz <admin@ezlgg.com>
2017-06-23 11:15:45 -0400

    redoing with an object for the employee

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
+function person(url, fullName, dob, user, place, homeland, mail, cell, home) {
+    this.image = url;
+    this.fullName = fullName;
+    this.birthday = dob;
+    this.username = user;
+    this.city = place;
+    this.country = homeland;
+    this.email = mail;
+    this.telephone = cell;
+    this.address = home;
+}
+
+let employeesList = [];
+let searchArray = [];
+
 function createDirectory(index, image, fullName, username, city, country) {
   let cardSetup = '<div class="employee"><a href="#" data-toggle="modal"';
   let dataTarget = 'data-target="#myModal' + index + '">';
@@ -9,42 +24,6 @@ function createDirectory(index, image, fullName, username, city, country) {
   return cardSetup + dataTarget + picture + nameP + usernameP + cityP
 }
 
-function leftOrRight() {
-  $('.modal').each(function(){
-    let currentModal = $(this);
-    //click next
-    currentModal.find('.btn-next').click(function(){
-      currentModal.modal('hide');
-      currentModal.closest('.modal').nextAll('.modal').first().modal('show'); 
-    });
-    //click prev
-    currentModal.find('.btn-prev').click(function(){
-      currentModal.modal('hide');
-      currentModal.closest('.modal').prevAll('.modal').first().modal('show');
-    });
-  });
-}
-
-function createModal(index, image, fullName, email, city, username, telephone, address, birthday) {
-  let modalSetup = '<div class="modal fade" id="myModal' + index + '" tabindex="-1" role="dialog" aria-hidden="false"><div class="modal-dialog" role="document"><div class="modal-content">';
-  let modalHeader = '<div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="false">&times;</span></div><div class="modal-body">';
-  let modalImage = '<img src="' + image + '" width="190" height="190" class="img-circle">';
-  let modalName = '<h3 class="modal-title"><b>Full Name :</b> ' + fullName + '</h3>';
-  let modalEmail = '<p class="modal-title"><b>Email : </b>' + email + '</p>';
-  let modalCity = '<p class="modal-title"><b>City : </b>' + city + '</p><hr>';
-  let modalUsername = '<p class="modal-title"><b>Username : </b>' + username + '</p>';
-  let modalTelephone = '<p class="modal-title"><b>Cell : </b>' + telephone + '</p>';
-  let modalAddress = '<p class="modal-title"><b>Address : </b>' + address + '</p>';
-  let modalBirthday = '<p class="modal-title"><b>Birthday : </b>' + birthday + '</p></div>';
-  let modalFooter = '<div class="modal-footer">';
-  let leftButton = '<button type="button" <a href="#" id="leftButton" class="btn btn-info btn-prev" data-toggle="modal data-target="#myModal' + (index - 1) + '">Left</a></button>';
-  let rightButton = '<button type="button" <a href="#" id="rightButton" class="btn btn-info btn-next" data-toggle="modal data-target="#myModal' + (index + 1) + '">Right</a></button>';
-  let closeButton = '<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></div></div></div></div></div>';
-  leftOrRight();
-  return modalSetup + modalHeader + modalImage + modalName + modalEmail + modalCity + modalUsername + modalTelephone + modalAddress + modalBirthday + modalFooter + leftButton + rightButton + closeButton
-}
-let searchArray = [];
-
 $.ajax({
   url: 'https://randomuser.me/api/?results=12',
   dataType: 'json',
@@ -55,6 +34,7 @@ $.ajax({
     function capitalize(name) {
     	return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
     }
+
     $.each(employees, function (index, value) {
     	fullName = capitalize(employees[index].name.first) + ' ' + capitalize(employees[index].name.last);
     	email = employees[index].email;
@@ -66,43 +46,55 @@ $.ajax({
       address = employees[index].location.street + ' ' + city + ' ' + employees[index].location.state + ', ' + country + employees[index].location.postcode;
       birthday = employees[index].dob;
 
-      searchArray.push(fullName);
-      searchArray.push(username);
-      console.log(searchArray);
+      searchArray.push(fullName.toLowerCase());
+      searchArray.push(username.toLowerCase());
+      employeesList.push(new person(image, fullName, birthday, username, city, country, email, telephone, address));
 
-        fullWrapper = createDirectory(index, image, fullName, username, city, country);
-
-        fullModal = createModal(index, image, fullName, email, city, username, telephone, address, birthday);
+      fullWrapper = createDirectory(index, image, fullName, username, city, country);
 
         $('#wrapper').append(fullWrapper);
-        $('#wrapper').append(fullModal);
-        // if(name.length > 15) {
-        //   $('.name').style.fontSize = '1.1rem';
-        // } 
-        // if(city.length > 15) {
-        //     $('.city').style.fontSize = '1rem';
-        // }
+        
     })
+
+    // function leftOrRight() {
+//   $('.modal').each(function(){
+//     let currentModal = $(this);
+//     //click next
+//     currentModal.find('.btn-next').click(function(){
+//       currentModal.modal('hide');
+//       currentModal.closest('.modal').nextAll('.modal').first().modal('show'); 
+//     });
+//     //click prev
+//     currentModal.find('.btn-prev').click(function(){
+//       currentModal.modal('hide');
+//       currentModal.closest('.modal').prevAll('.modal').first().modal('show');
+//     });
+//   });
+// }
+
+    // const $selection = $('#selectbasic');
+    // const $search = $('#searchBar');
+    // const $buttonSearch = $('#searchButton');
+    // // const $employee = $('.employee');
+
+    // $buttonSearch.click(function(event) {
+    //   let searchWord = $search.val().toLowerCase();
+    //   $search.val('');
+    //   let $employee = $('.emoloyee');
+    //   $.each(searchArray, function ( index, element ) {
+    //     let testString = searchArray.indexOf(searchWord);
+    //     console.log(testString);
+    //     if (testString < 0) {
+    //       console.log('inside if');
+    //       $employee.hide();
+    //     }
+    //   });
+    // }); 
   }
 });
 
-//search feature
+//search feature $.inArray(searchWord, searchArray)
 
-const selection = $('#selectbasic');
-const search = $('#searchBar');
-const buttonSearch = $('#searchButton');
-
-buttonSearch.click(function(event) {
-  let searchWord = search.val();
-  search.val('');
-  $.each(searchArray, function(index, el) {
-    if (searchArray[index].indexOf('searchWord') < 0) {
-      $('.employee[index]').hide();
-    }
-    
-  });
-
-});
 
 
 //console.log('');
