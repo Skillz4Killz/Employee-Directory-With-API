eb80fe73112e0c69466453fdc61366b4704b88bb
Skillz <admin@ezlgg.com>
2017-06-23 20:09:35 -0400

    finished search feature, needs final revision before submitting

--- js/app.js
+++ js/app.js
@@ -24,17 +24,53 @@ function createDirectory(index, image, fullName, username, city, country) {
   return cardSetup + dataTarget + picture + nameP + usernameP + cityP
 }
 
+function createModal(index, image, fullName, email, city, username, telephone, address, birthday) {
+  let modalSetup = '<div class="modal fade" id="myModal' + index + '" tabindex="-1" role="dialog" aria-hidden="false"><div class="modal-dialog" role="document"><div class="modal-content">';
+  let modalHeader = '<div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="false">&times;</span></div><div class="modal-body">';
+  let modalImage = '<img src="' + image + '" width="190" height="190" class="img-circle">';
+  let modalName = '<h3 class="modal-title"><b>Full Name :</b> ' + fullName + '</h3>';
+  let modalEmail = '<p class="modal-title"><b>Email : </b>' + email + '</p>';
+  let modalCity = '<p class="modal-title"><b>City : </b>' + city + '</p><hr>';
+  let modalUsername = '<p class="modal-title"><b>Username : </b>' + username + '</p>';
+  let modalTelephone = '<p class="modal-title"><b>Cell : </b>' + telephone + '</p>';
+  let modalAddress = '<p class="modal-title"><b>Address : </b>' + address + '</p>';
+  let modalBirthday = '<p class="modal-title"><b>Birthday : </b>' + birthday + '</p></div>';
+  let modalFooter = '<div class="modal-footer">';
+  let leftButton = '<button type="button" <a href="#" id="leftButton" class="btn btn-info btn-prev" data-toggle="modal data-target="#myModal' + (index - 1) + '">Left</a></button>';
+  let rightButton = '<button type="button" <a href="#" id="rightButton" class="btn btn-info btn-next" data-toggle="modal data-target="#myModal' + (index + 1) + '">Right</a></button>';
+  let closeButton = '<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></div></div></div></div></div>';
+  leftOrRight();
+  return modalSetup + modalHeader + modalImage + modalName + modalEmail + modalCity + modalUsername + modalTelephone + modalAddress + modalBirthday + modalFooter + leftButton + rightButton + closeButton
+}
+
+function leftOrRight() {
+  $('.modal').each(function(){
+    let currentModal = $(this);
+    //click next
+    currentModal.find('.btn-next').click(function(){
+      currentModal.modal('hide');
+      currentModal.closest('.modal').nextAll('.modal').first().modal('show'); 
+    });
+    //click prev
+    currentModal.find('.btn-prev').click(function(){
+      currentModal.modal('hide');
+      currentModal.closest('.modal').prevAll('.modal').first().modal('show');
+    });
+  });
+}
+
+let image, fullName, email, city, username, fullWrapper, modal, telephone, address, birthday, country, fullModal, card;
+
+function capitalize(name) {
+      return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+    }
+
 $.ajax({
   url: 'https://randomuser.me/api/?results=12',
   dataType: 'json',
   success: function(data) {
     let employees = data.results;
-    let image, fullName, email, city, username, fullWrapper, modal, telephone, address, birthday, country;
     
-    function capitalize(name) {
-    	return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
-    }
-
     $.each(employees, function (index, value) {
     	fullName = capitalize(employees[index].name.first) + ' ' + capitalize(employees[index].name.last);
     	email = employees[index].email;
@@ -46,36 +82,25 @@ $.ajax({
       address = employees[index].location.street + ' ' + city + ' ' + employees[index].location.state + ', ' + country + employees[index].location.postcode;
       birthday = employees[index].dob;
 
-      searchArray.push(fullName.toLowerCase());
+      searchArray.push(employees[index].name.first.toLowerCase());
+      searchArray.push(employees[index].name.last.toLowerCase());
       searchArray.push(username.toLowerCase());
+
       employeesList.push(new person(image, fullName, birthday, username, city, country, email, telephone, address));
 
       fullWrapper = createDirectory(index, image, fullName, username, city, country);
+      fullModal = createModal(index, image, fullName, email, city, username, telephone, address, birthday);
 
         $('#wrapper').append(fullWrapper);
+        $('#wrapper').append(fullModal);
         
     })
-
-    // function leftOrRight() {
-//   $('.modal').each(function(){
-//     let currentModal = $(this);
-//     //click next
-//     currentModal.find('.btn-next').click(function(){
-//       currentModal.modal('hide');
-//       currentModal.closest('.modal').nextAll('.modal').first().modal('show'); 
-//     });
-//     //click prev
-//     currentModal.find('.btn-prev').click(function(){
-//       currentModal.modal('hide');
-//       currentModal.closest('.modal').prevAll('.modal').first().modal('show');
-//     });
-//   });
-// }
+card = document.getElementsByClassName('employee');
 
     // const $selection = $('#selectbasic');
     // const $search = $('#searchBar');
     // const $buttonSearch = $('#searchButton');
-    // // const $employee = $('.employee');
+    // const $employee = $('.employee');
 
     // $buttonSearch.click(function(event) {
     //   let searchWord = $search.val().toLowerCase();
@@ -93,8 +118,21 @@ $.ajax({
   }
 });
 
-//search feature $.inArray(searchWord, searchArray)
+const search = document.getElementById('searchBar');
+const buttonSearch = document.getElementById('searchButton');
 
-
-
-//console.log('');
+buttonSearch.addEventListener('click', () => {
+  let searchWord = search.value.toLowerCase();
+  search.value = '';
+  for (let i = 0; i < employeesList.length; i++ ) {
+    card[i].style.display = 'none';
+  }
+  for (let i = 0; i < employeesList.length; i++) {
+    let testName = employeesList[i].fullName.toLowerCase();
+     testName = testName.indexOf(searchWord);
+    let testUsername = employeesList[i].username.indexOf(searchWord);
+    if (testName >= 0 || testUsername >= 0) {
+      card[i].style.display = '';
+    }
+  }
+})
